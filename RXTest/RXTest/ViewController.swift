87c28a8eb9c86b28f0011472c42bef8638405df4
姬冠杰 <15121139605@163.com>
2018-12-04 13:51:03 +0800

    update1

--- RXTest/RXTest/ViewController.swift
+++ RXTest/RXTest/ViewController.swift
@@ -7,14 +7,28 @@
 //
 
 import UIKit
+import RxSwift
+import RxCocoa
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var acountTextField: UITextField!
+    @IBOutlet weak var passTextField: UITextField!
+    @IBOutlet weak var countLabel: UILabel!
+    @IBOutlet weak var summitBt: UIButton!
+    let dispose = DisposeBag()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
     }
-
-
 }
 
+extension ViewController {
+    // 账号密码绑定到一起
+    func test1() {
+        Observable.combineLatest(acountTextField.rx.text.orEmpty, passTextField.rx.text.orEmpty) { (test1, test2) -> String in
+            return "账号:\(test1) 密码:\(test2)"
+            }.bind(to: countLabel.rx.text).disposed(by: dispose)
+    }
+}
